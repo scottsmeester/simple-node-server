936f11639164274dc22ea628b30b21e181b169fb
scottsmeester <scott@smeester.com>
2015-02-23 12:53:45 -0700

    fixed syncronous

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,10 +1,20 @@
-function start() {
+var exec = require("child_process").exec;
+
+function start(response) {
   console.log("Request handler 'start' was called.");
-  return 'Hello Start';
+  var content = 'empty';
+  exec("ls -lah", function (error, stdout, stderr) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write(stdout);
+    response.end();
+  });
+  return content;
 }
-function upload() {
+function upload(response) {
   console.log("Request handler 'upload' was called.");
-  return 'Hello upload';
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello Upload");
+  response.end();
 }
 exports.start = start;
 exports.upload = upload;
